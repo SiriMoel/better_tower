0f3a2eac8d9bcc1f160df28db1d261376db09245
Mir Sole <zburn101@gmail.com>
2022-07-11 21:51:41 +1000

    basic stuff

---
+++ init.lua
@@ -0,0 +1,19 @@
+local nxml = dofile_once("mods/better_tower/lib/nxml.lua")
+
+function OnModInit()
+
+end
+
+function OnModPostInit()
+
+end
+
+function OnPlayerSpawned( player_entity )
+end
+
+function OnWorldPostUpdate()
+end
+
+function OnPlayerDied()
+
+end
