0942b76aa4524ddef15b34cd26e11d242281fc74
Mir Sole <zburn101@gmail.com>
2022-07-14 21:39:54 +1000

    another failed attempt, more work on it tmr

--- data/entities/buildings/tower_lock/unlock.lua
+++ data/entities/buildings/tower_lock/unlock.lua
@@ -8,22 +8,21 @@ function collision_trigger(colliding_entity_id)
 
     --keys
     if (EntityHasTag(colliding_entity_id, "tower_key_1")) == true and (EntityHasTag(lock, "in_tower_key_1")) == false then
-        ConvertMaterialOnAreaInstantly(x, y, 50, 50, CellFactory_GetType("towerlock_1_locked"), CellFactory_GetType("radioactive_liquid"), false, false)
+        --ConvertMaterialOnAreaInstantly(x, y, 50, 50, CellFactory_GetType("towerlock_1_locked"), CellFactory_GetType("radioactive_liquid"), false, false)
+        --EntityAddComponent(lock, "MagicConvertMaterialComponent", { _enabled="1", kill_when_finished="0", from_material="towerlock_1_locked", steps_per_frame="5", to_material="blood", radius="150" })
+
         EntityAddTag(lock, "in_tower_key_1")
         EntityKill(colliding_entity_id)
     end
     if (EntityHasTag(colliding_entity_id, "tower_key_2")) == true and (EntityHasTag(lock, "in_tower_key_2")) == false then
-        ConvertMaterialOnAreaInstantly(x, y, 50, 50, CellFactory_GetType("towerlock_2_locked"), CellFactory_GetType("radioactive_liquid"), false, false)
         EntityAddTag(lock, "in_tower_key_2")
         EntityKill(colliding_entity_id)
     end
     if (EntityHasTag(colliding_entity_id, "tower_key_3")) == true and (EntityHasTag(lock, "in_tower_key_3")) == false then
-        ConvertMaterialOnAreaInstantly(x, y, 50, 50, CellFactory_GetType("towerlock_3_locked"), CellFactory_GetType("radioactive_liquid"), false, false)
         EntityAddTag(lock, "in_tower_key_3")
         EntityKill(colliding_entity_id)
     end
     if (EntityHasTag(colliding_entity_id, "tower_key_4")) == true and (EntityHasTag(lock, "in_tower_key_4")) == false then
-        ConvertMaterialOnAreaInstantly(x, y, 50, 50, CellFactory_GetType("towerlock_4_locked"), CellFactory_GetType("radioactive_liquid"), false, false)
         EntityAddTag(lock, "in_tower_key_4")
         EntityKill(colliding_entity_id)
     end
